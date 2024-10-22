e90fb669533d07f1835b824a686d5e859097f7cb
ngendajo <52460758+ngendajo@users.noreply.github.com>
2024-10-22 19:55:24 +0200

    my first commit to this project

--- apis/serializers.py
+++ apis/serializers.py
@@ -1,30 +1,24 @@
 from rest_framework import serializers
-from .models import User, Student, SpecialFood, Prescription
+from .models import *
 
-# User serializer
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'role']
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'groups', 'user_permissions']
 
-# Student serializer
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
         fields = ['id', 'first_name', 'last_name', 'student_id']
 
-# SpecialFood serializer
 class SpecialFoodSerializer(serializers.ModelSerializer):
     class Meta:
         model = SpecialFood
         fields = ['id', 'name', 'description']
 
-# Prescription serializer
 class PrescriptionSerializer(serializers.ModelSerializer):
     student = StudentSerializer()
     special_foods = SpecialFoodSerializer(many=True)
-    issued_by = UserSerializer()
-    given_by = UserSerializer()
 
     class Meta:
         model = Prescription
--- apis/views.py
+++ apis/views.py
@@ -1,20 +1,62 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from .models import User, Student, SpecialFood, Prescription
-from .serializers import StudentSerializer, UserSerializer, SpecialFoodSerializer, PrescriptionSerializer
-# Create your views here.
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
+from .models import *
+from .serializers import *
+
 class UserViewSet(viewsets.ModelViewSet):
-  queryset = User.objects.all()
-  serializer_class = UserSerializer
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        try:
+            return super().create(request, *args, **kwargs)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
 class StudentViewSet(viewsets.ModelViewSet):
-  queryset = Student.objects.all()
-  serializer_class = StudentSerializer
-  
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def create(self, request, *args, **kwargs):
+        try:
+            return super().create(request, *args, **kwargs)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
 class SpecialFoodViewSet(viewsets.ModelViewSet):
     queryset = SpecialFood.objects.all()
     serializer_class = SpecialFoodSerializer
 
+    def create(self, request, *args, **kwargs):
+        try:
+            return super().create(request, *args, **kwargs)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
 class PrescriptionViewSet(viewsets.ModelViewSet):
     queryset = Prescription.objects.all()
     serializer_class = PrescriptionSerializer
+
+    def perform_create(self, serializer):
+        try:
+            student_data = self.request.data.get('student')
+            special_foods_data = self.request.data.get('special_foods')
+
+            # Handle nested data for student and special_foods
+            if student_data:
+                student, created = Student.objects.get_or_create(**student_data)
+                serializer.save(student=student)
+
+            if special_foods_data:
+                special_foods = SpecialFood.objects.filter(id__in=[food['id'] for food in special_foods_data])
+                serializer.save(special_foods=special_foods)
+
+        except Exception as e:
+            raise NotFound({"error": str(e)})
+
+    def create(self, request, *args, **kwargs):
+        try:
+            return super().create(request, *args, **kwargs)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
